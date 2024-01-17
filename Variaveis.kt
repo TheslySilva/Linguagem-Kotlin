aa5008d07253a048402f5baf98754d0170af1a6c
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2024-01-17 10:35:55 -0300

    update Variaveis.kt

---
+++ Variaveis.kt
@@ -0,0 +1,8 @@
+fun main() {
+  // Val é usado para declarar uma Constante imutavel
+  val nome = "Thesly"
+  // Var é usado para declarar uma Variavel mutavel
+  var idade = 21
+  
+  println("Meu nome é : " + nome + "e tenho " + idade + "anos")
+}
