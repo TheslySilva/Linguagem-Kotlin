850d539ef495ab79b5b06aa0adafa9cfecd2b2c4
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2024-01-17 10:37:32 -0300

    update Variaveis.kt

--- Variaveis.kt
+++ Variaveis.kt
@@ -3,6 +3,6 @@ fun main() {
   val nome = "Thesly"
   // Var é usado para declarar uma Variavel mutavel
   var idade = 21
-  
+  // Imprimindo as variaveis
   println("Meu nome é " + nome + " e tenho " + idade + " anos")
 }
