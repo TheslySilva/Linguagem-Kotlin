87a5eaad98b24baa64945bdcd394d197f4194de4
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2024-01-19 12:02:34 -0300

    update Listas.kt

---
+++ Listas.kt
@@ -0,0 +1,11 @@
+fun main() {
+
+    // Lista apenas de leitura
+    val formasLeitura = listOf("Triangulo", "Retangulo", "Quadrado")
+    println(formasLeitura)
+    
+    // Lista que pode mudar 
+    val formasMutaveis : MutableList <String> = mutableListOf("Triangulo", "Retangulo", "Quadrado")
+    println(formasMutaveis)
+    
+}
